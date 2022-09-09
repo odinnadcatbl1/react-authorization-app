d4a5c0f589a7803b53e2a43a88cb9007c94369ae
Salavat F <salmrproperfahr@mail.ru>
2022-09-09 19:45:53 +0300

    done username input

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Register from "./Register";
+
+const App = () => {
+    return (
+        <main className="app">
+            <Register />
+        </main>
+    );
+};
+
+export default App;
