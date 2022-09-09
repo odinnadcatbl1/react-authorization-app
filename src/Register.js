be377195b5c9437c9429eb89ff0fd64f360226e4
Salavat F <salmrproperfahr@mail.ru>
2022-09-09 20:53:36 +0300

    done password and confirm inputs

--- src/Register.js
+++ src/Register.js
@@ -19,14 +19,14 @@ const Register = () => {
 
     const [pwd, setPwd] = useState("");
     const [validPwd, setValidPwd] = useState(false);
-    const [pwdFocus, setUserFPwd] = useState(false);
+    const [pwdFocus, setPwdFocus] = useState(false);
 
     const [matchPwd, setMatchPwd] = useState("");
     const [validMatch, setValidMatch] = useState(false);
     const [matchFocus, setMatchFocus] = useState(false);
 
     const [errMsg, setErrMsg] = useState("");
-    const [success, setSucces] = useState(false);
+    const [success, setSuccess] = useState(false);
 
     useEffect(() => {
         userRef.current.focus();
@@ -101,6 +101,80 @@ const Register = () => {
                     Must begin with a letter.
                     <br />
                     Letters, numbers, underscores, hyphens allowed.
+                </p>
+
+                <label htmlFor="password">
+                    Password:
+                    <FontAwesomeIcon
+                        icon={faCheck}
+                        className={validPwd ? "valid" : "hide"}
+                    />
+                    <FontAwesomeIcon
+                        icon={faTimes}
+                        className={validPwd || !pwd ? "hide" : "invalid"}
+                    />
+                </label>
+                <input
+                    type="password"
+                    id="password"
+                    onChange={(e) => setPwd(e.target.value)}
+                    value={pwd}
+                    required
+                    aria-invalid={validPwd ? "false" : "true"}
+                    aria-describedby="pwdnote"
+                    onFocus={() => setPwdFocus(true)}
+                    onBlur={() => setPwdFocus(false)}
+                />
+                <p
+                    id="pwdnote"
+                    className={
+                        pwdFocus && !validPwd ? "instructions" : "offscreen"
+                    }
+                >
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    8 to 24 characters.
+                    <br />
+                    Must include uppercase and lowercase letters, a number and a
+                    special character.
+                    <br />
+                    Allowed special characters:{" "}
+                    <span aria-label="exclamation mark">!</span>{" "}
+                    <span aria-label="at symbol">@</span>{" "}
+                    <span aria-label="hashtag">#</span>{" "}
+                    <span aria-label="dollar sign">$</span>{" "}
+                    <span aria-label="percent">%</span>
+                </p>
+
+                <label htmlFor="confirm_pwd">
+                    Confirm Password:
+                    <FontAwesomeIcon
+                        icon={faCheck}
+                        className={validMatch && matchPwd ? "valid" : "hide"}
+                    />
+                    <FontAwesomeIcon
+                        icon={faTimes}
+                        className={validMatch || !matchPwd ? "hide" : "invalid"}
+                    />
+                </label>
+                <input
+                    type="password"
+                    id="confirm_pwd"
+                    onChange={(e) => setMatchPwd(e.target.value)}
+                    value={matchPwd}
+                    required
+                    aria-invalid={validMatch ? "false" : "true"}
+                    aria-describedby="confirmnote"
+                    onFocus={() => setMatchFocus(true)}
+                    onBlur={() => setMatchFocus(false)}
+                />
+                <p
+                    id="confirmnote"
+                    className={
+                        matchFocus && !validMatch ? "instructions" : "offscreen"
+                    }
+                >
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    Must match the first password input field.
                 </p>
             </form>
         </section>
