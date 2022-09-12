c42f63485bbaf0103dca5b8f767bee0414de7089
Salavat F <salmrproperfahr@mail.ru>
2022-09-12 11:39:23 +0300

    done components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import Register from "./Register";
 import Login from "./Login";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
     return (
-        <main className="App">
-            <Login />
-        </main>
+        <Routes>
+            <Route path="/" element={<Layout />} />
+            {/* public routes */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/linkpage" element={<LinkPage />} />
+            <Route path="/unauthorized" element={<Unauthorized />} />
+
+            {/* private routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/editor" element={<Editor />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/lounge" element={<Lounge />} />
+
+            {/* catch all */}
+            <Route path="*" element={<Missing />} />
+        </Routes>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App";
 import "./index.css";
 
@@ -8,8 +10,12 @@ import { AuthProvider } from "./context/AuthProvider";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <AuthProvider>
-            <App />
-        </AuthProvider>
+        <BrowserRouter>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/*" element={<App />} />
+                </Routes>
+            </AuthProvider>
+        </BrowserRouter>
     </React.StrictMode>
 );
