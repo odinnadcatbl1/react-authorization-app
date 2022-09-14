63f6adf1b781305870d32b64188348e557c3ddc9
Salavat F <salmrproperfahr@mail.ru>
2022-09-14 14:11:11 +0300

    rewrite requireauth with roles

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -1,12 +1,14 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
-const RequireAuth = () => {
+const RequireAuth = ({ allowedRoles }) => {
     const { auth } = useAuth();
     const location = useLocation();
 
-    return auth?.user ? (
+    return auth?.roles?.find((role) => allowedRoles?.includes(role)) ? (
         <Outlet />
+    ) : auth?.user ? (
+        <Navigate to="/unauthorized" state={{ from: location }} replace />
     ) : (
         <Navigate to="/login" state={{ from: location }} replace />
     );
