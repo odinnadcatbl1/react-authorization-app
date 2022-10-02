3b9c844aedfb415084888c4728f20112b9ffcdfb
Salavat F <salmrproperfahr@mail.ru>
2022-10-02 11:56:11 +0300

    handling an expired refresh token

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Users = () => {
     const [users, setUsers] = useState();
     const axiosPrivate = useAxiosPrivate();
+    const navigate = useNavigate();
+    const location = useLocation();
 
     useEffect(() => {
         let isMounted = true;
@@ -19,6 +22,10 @@ const Users = () => {
                 isMounted && setUsers(response.data);
             } catch (err) {
                 console.log(err);
+                navigate("/login", {
+                    state: { from: location },
+                    replace: true,
+                });
             }
         };
 
