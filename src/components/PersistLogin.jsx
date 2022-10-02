934e23680eb5ba8fbae2598a6a59d8d7ff0990d1
Salavat F <salmrproperfahr@mail.ru>
2022-10-02 12:40:13 +0300

    upgrade persist login component

--- src/components/PersistLogin.jsx
+++ src/components/PersistLogin.jsx
@@ -6,7 +6,7 @@ import useAuth from "../hooks/useAuth";
 const PersistLogin = () => {
     const [isLoading, setIsLoading] = useState(true);
     const refresh = useRefreshToken();
-    const { auth } = useAuth();
+    const { auth, persist } = useAuth();
 
     useEffect(() => {
         const verifyRefreshToken = async () => {
@@ -22,7 +22,11 @@ const PersistLogin = () => {
         !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
     }, []);
 
-    return <>{isLoading ? <p>Loading...</p> : <Outlet />}</>;
+    return (
+        <>
+            {!persist ? <Outlet /> : isLoading ? <p>Loading...</p> : <Outlet />}
+        </>
+    );
 };
 
 export default PersistLogin;
